18ba93896a277dbf8959c082d89996808c50c581
Kay-raph <kayraph3d@gmail.com>
2020-12-18 14:38:59 +0300

    added hurmbager menu and changed logo

--- src/js/findsignup.js
+++ src/js/findsignup.js
@@ -1,4 +1,48 @@
-//Email validation
+   // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        var firebaseConfig = {
+            apiKey: "AIzaSyBWTs5YA3POIice1TXno2GO9tYGHFJTjk0",
+            authDomain: "crimson-33.firebaseapp.com",
+            databaseURL: "https://crimson-33.firebaseio.com",
+            projectId: "crimson-33",
+            storageBucket: "crimson-33.appspot.com",
+            messagingSenderId: "801019593695",
+            appId: "1:801019593695:web:e9b9b1ea0b77bc3b7d3a6a",
+            measurementId: "G-QNGLP6GJSR"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+//Initiate database and save data
+document.getElementById("fsignup").addEventListener("submit",function(e){
+    e.preventDefault();
+    var email =document.getElementById("ema");
+    var psw =document.getElementById("password");
+    firebase.auth().createUserWithEmailAndPassword(email.value, psw.value)
+    .then(function(response){
+        alert("Successfully Signed Up");
+        console.log(response);
+        firebase.database().ref("Sign Up").push({
+            email: ema.value,
+            psw: password.value,
+            email:firebase.auth().currentUser.email
+        })
+        firebase.auth().signOut().then(function(){
+          email.value="";
+        psw.value="";   
+        })
+       
+    })
+    .catch(function(error){
+        //Error Handling
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+    
+});
+
 var emailField = document.getElementById("email");
 var button = document.getElementById("submit");
 var response = document.getElementById("response");
@@ -6,7 +50,7 @@ var response = document.getElementById("response");
 button.addEventListener("click", function () {
   var email = emailField.value;
   if (validateEmail(email)) {
-    response.innerHTML = ""
+    response.innerHTML = "Successfully Signed Up"
   } else {
     response.innerHTML = "Sorry, Enter valid Email!! 😩";
   }
@@ -16,49 +60,5 @@ function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-//End of email validation
-//Password Validation
-$(document).ready(function(){
-    var $submitBtn = $("#form input[type='submit']");
-    var $passwordBox = $("#password");
-    var $confirmBox = $("#confirm_password");
-    var $errorMsg =  $('<span id="error_msg">Password mismatch!!Try again.</span>');
 
-    // This is incase the user hits refresh - some browsers will maintain the disabled state of the button.
-    $submitBtn.removeAttr("disabled");
-
-    function checkMatchingPasswords(){
-        if($confirmBox.val() != "" && $passwordBox.val != ""){
-            if( $confirmBox.val() != $passwordBox.val() ){
-                $submitBtn.attr("disabled", "disabled");
-                $errorMsg.insertAfter($confirmBox);
-            }
-        }
-    }
-
-    function resetPasswordError(){
-        $submitBtn.removeAttr("disabled");
-        var $errorCont = $("#error_msg");
-        if($errorCont.length > 0){
-            $errorCont.remove();
-        }  
-    }
-            $("#confirm_password, #password")
-             .on("keydown", function(e){
-                /* only check when the tab or enter keys are pressed
-                 * to prevent the method from being called needlessly  */
-                if(e.keyCode == 13 || e.keyCode == 9) {
-                    checkMatchingPasswords();
-                }
-             })
-             .on("blur", function(){                    
-                // also check when the element looses focus (clicks somewhere else)
-                checkMatchingPasswords();
-            })
-            .on("focus", function(){
-                // reset the error message when they go to make a change
-                resetPasswordError();
-            })
-
-    });
     
